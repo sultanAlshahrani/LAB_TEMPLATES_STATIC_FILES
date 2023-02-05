6cd7f79efc55a54848828e84f02a226de539ba4d
sultan <sultan_33_886@outlook.sa>
2023-02-05 09:52:25 +0300

    lab_cookies

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sultan\python-camp\LAB_TEMPLATES_STATIC_FILES\venv
+set VIRTUAL_ENV=C:\Users\sultan\python-camp\LAB_COOKIES\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
